dcf83288c903e52eb995c830c8236eba366547a5
fummicc1-sub <fummiya989@gmail.com>
2018-12-09 01:05:59 +0900

    commit

--- Assets/Scripts/Result/FailedResult.cs
+++ Assets/Scripts/Result/FailedResult.cs
@@ -6,33 +6,16 @@ using UnityEngine.UI;
 public class FailedResult : MonoBehaviour
 {
 
-    public CreateComment createComment; // コメントの表示するタイミングを調整するために宣言。
+    public Camera target;
 
-    public Text label;
-    public Image image;
-
-    // Use this for initialization
-    void Start()
-    {
-        createComment.enabled = false;
-        label.enabled = false;
-        image.enabled = false;
-        //　1.5秒遅らせる
-        StartCoroutine(HideObj());
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
 
     }
 
-    IEnumerator HideObj()
+    private void Update()
     {
-        yield return new WaitForSeconds(1.5f);
-        image.enabled = true;
-        label.enabled = true;
-        createComment.enabled = true;
+        target.transform.position += new Vector3(0, 0, 0.01f);
     }
 
 }
