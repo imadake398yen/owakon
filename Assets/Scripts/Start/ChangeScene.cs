cba9c1437661afa293628fb3b8e864dd934207fd
fummicc1-sub <fummiya989@gmail.com>
2018-12-08 23:20:17 +0900

    create ChangeSceneScript

---
+++ Assets/Scripts/Start/ChangeScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void ChangeSceneTo(string sceneName)
+    {
+        _ChangeSceneTo(sceneName);
+    }
+
+    private static void _ChangeSceneTo(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
