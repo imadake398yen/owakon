6f023f8f33ea8c6c14716952ed39d5b84afe26c8
ShirasuSalaD <hihihipoke@gmail.com>
2018-12-08 23:01:08 +0900

    Animalの追加

--- Assets/Scripts/Common/Const.cs
+++ Assets/Scripts/Common/Const.cs
@@ -7,7 +7,7 @@
 
 	public enum StageState {
 		Create,
-		BraveManHomecoming
+		BraveManHomecoming,
 	}
 
 }
---
+++ Assets/Scripts/EnemyController/AnimalComeHit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalComeHit : Obstacle {
+
+    [SerializeField]
+    private int MaxChild = 5;
+    private GameObject BoidsChild;
+    private GameObject[] BoidsChildren;
+
+	// Use this for initialization
+    private void Start (){
+
+    }
+
+	// Update is called once per frame
+	public void Play () {
+        base.Play();
+        this.BoidsChildren = new GameObject[MaxChild];
+
+        for (int i = 0; i < this.MaxChild; i++){
+                this.BoidsChildren[i] = GameObject.Instantiate(BoidsChild) as GameObject;
+
+                this.BoidsChildren[i].transform.position = new Vector3(Random.Range(-50f, 50f),this.BoidsChild.transform.position.y,Random.Range(-50f, 50f));
+        }
+
+	}
+
+    void OnMouseDown(){
+        Play();
+    }
+
+}
