32c4919299b7c8b4626f01cf46a6b7676e9b0f6b
ShirasuSalaD <hihihipoke@gmail.com>
2018-12-09 01:27:52 +0900

    lowpolycowの追加 Storyシーンの追加

---
+++ Assets/Scenes/morimo-/FadeImage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeImage : MonoBehaviour {
+    float alfa;
+    float speed = 0.01f;
+    float red, green, blue;
+		public Image Slide;
+
+    void Start () {
+      red = Slide.color.r;
+      green = Slide.color.g;
+      blue = Slide.color.b;
+			alfa = Slide.color.a;
+
+			Slide.color = new Color(red, green, blue, alfa);
+    }
+
+    void Update () {
+      	alfa += speed;
+				// if(Input.GetMouseButton(0)){
+      	//   alfa -= speed * 2;
+				// }
+
+    }
+
+}
