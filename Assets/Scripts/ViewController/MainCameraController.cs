87d0ddd7d044914fea6e6f740454b5f9439dac05
falcon <imadake398yen@gmail.com>
2018-12-08 15:16:05 +0900

    Cameraの動きを修正

--- Assets/Scripts/ViewController/MainCameraController.cs
+++ Assets/Scripts/ViewController/MainCameraController.cs
@@ -2,13 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCameraController : MonoBehaviour {
+public class MainCameraController
+: SingletonMonoBehaviour<MainCameraController> {
+
+	[SerializeField] private Const.CameraState state = Const.CameraState.ChaseTarget;
+	public Const.CameraState State { get{ return state; } }
+	public void ToChaseState () { state = Const.CameraState.ChaseTarget; }
 
 	[SerializeField] private Transform target;
+	private float sensitivity = .001f;
 	private Vector3 offset = new Vector3(-10,20,-25);
+	private Vector3 lastPointerPosition, scrollDistance;
 
 	private void Update () {
-		transform.position = target.position + offset;
-		transform.LookAt(target.position);
+		if (Input.GetMouseButtonDown(0)) {
+			lastPointerPosition = Input.mousePosition;
+		}
+		if (Input.GetMouseButton(0)) {
+			if (Vector2.Distance(lastPointerPosition, Input.mousePosition) > 3) {
+				state = Const.CameraState.Scroll;
+				scrollDistance = Input.mousePosition - lastPointerPosition;
+			}
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			scrollDistance = Vector3.zero;
+		}
+		switch (state) {
+			case Const.CameraState.ChaseTarget:
+				var moveTarget = target.position + offset;
+				transform.position = Vector3.Lerp(
+					transform.position,
+					moveTarget,
+					3f * Time.deltaTime
+				);
+				transform.LookAt(target.position);
+				break;
+			case Const.CameraState.Scroll:
+				transform.position += new Vector3( scrollDistance.x, 0, scrollDistance.y ) * sensitivity;
+				break;
+			default: break;
+		}
 	}
 }
