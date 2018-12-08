f3fcdc8a00f1d9157849f8e9bc76fa3741cd8415
falcon <imadake398yen@gmail.com>
2018-12-08 13:46:34 +0900

    Characterにカメラが追従するように

---
+++ Assets/Scripts/ViewController/MainCameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour {
+
+	[SerializeField] private Transform target;
+	private Vector3 offset = new Vector3(-10,20,-25);
+
+	private void Update () {
+		transform.position = target.position + offset;
+		transform.LookAt(target.position);
+	}
+}
