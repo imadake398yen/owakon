907e506ab3295502e073ae26fc26a274a1dfc481
fummicc1-sub <fummiya989@gmail.com>
2018-12-08 22:30:40 +0900

    commit

---
+++ Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour {
+
+    public GameObject player;
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (animator.GetBool("isAliving"))
+        {
+            player.transform.position += Vector3.forward * 0.01f;
+        }
+    }
+}
---
+++ Assets/Scripts/CreateComment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateComment : MonoBehaviour
+{
+
+    public Text label;
+
+    string[] good = {
+        "まだこんなところで死ぬわけにはいかない…",
+        "まだまだ戦えるぜ",
+};
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))//A
+        {
+            //StartCoroutine(setSpeech(good[UnityEngine.Random.Range(0, good.Length-1)]));
+            StartCoroutine(setSpeech(good[0]));
+        }
+    }
+
+    IEnumerator setSpeech(string text)
+    {
+        for (int i = 0; i <= text.Length; i++)
+        {
+            label.text = text.Substring(0, i);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
+
+
