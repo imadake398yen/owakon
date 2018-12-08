f309d2954647c2f4b8fa7a4c1ec6ab534be32f51
kiriko akashi <suzuranc841@gmail.com>
2018-12-08 12:10:41 +0900

    delete asset

---
+++ Assets/Scripts/BuildingController/buildingFalldown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingFalldown : MonoBehaviour {
+
+    private Rigidbody b_rigidbody = null;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        b_rigidbody.AddTorque(transform.right, ForceMode.Force);
+    }
+}
