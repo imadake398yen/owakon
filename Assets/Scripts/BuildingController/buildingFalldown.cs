79444283ed423f3104abfd903b585cac4f632e85
kiriko akashi <suzuranc841@gmail.com>
2018-12-08 12:27:57 +0900

    Cube倒れる

--- Assets/Scripts/BuildingController/buildingFalldown.cs
+++ Assets/Scripts/BuildingController/buildingFalldown.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class buildingFalldown : MonoBehaviour {
 
+    [SerializeField]
+    private float b_torqueForce = 0.0f;
+
     private Rigidbody b_rigidbody = null;
 
     // Use this for initialization
+    private void Awake()
+    {
+        b_rigidbody = GetComponent<Rigidbody>();
+
+    }
     void Start () {
-		
-	}
+
+        Vector3 rightVec = transform.right;
+        Vector3 torque = rightVec * b_torqueForce;
+        b_rigidbody.AddTorque(torque, ForceMode.Force);
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        b_rigidbody.AddTorque(transform.right, ForceMode.Force);
     }
 }
